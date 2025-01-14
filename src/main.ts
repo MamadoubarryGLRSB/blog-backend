7221afd1c7b5d2a0bee50ed8bcd6896a9b1e962f
MamadoubarryGLRSB <mamadou.barry1@ism.edu.sn>
2025-01-14 14:33:34 +0100

    fix: resolve CORS issue

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,13 @@ async function bootstrap() {
     .setVersion('1.0')
     .addBearerAuth()
     .build();
+  // Configuration CORS
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
